7cc241716d29461b1847d50dc27a89af26f9e30a
Nassim <nassim.haddad.pro@gmail.com>
2021-10-13 11:25:58 +0200

    Display pin owner and add getFullName method for user's full name generation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Entity\Traits\Timestampable;
+use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -204,5 +205,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         }
 
         return $this;
+    }
+
+    /** CUSTOM **/
+    public function getFullName(): string
+    {
+        return $this->getFirstName(). ' ' . $this->getLastName();
     }
 }
